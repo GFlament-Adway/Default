accde322c6b5ad3314a56307ca7fd6b782673024
GFlament <guillaume.flament@eleve.ensai.fr>
2022-03-10 11:40:54 +0100

    last update

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def generate_default_times(intensity = 0.2, censure_intensity = 0.08, n_obs=100):
+    times = np.random.exponential(1/intensity, n_obs)
+    censures = np.random.exponential(1/censure_intensity, n_obs)
+    return [min(times[k], censures[k]) for k in range(n_obs)], [1 if times[k] < censures[k] else 0for k in range(n_obs)]
 
+def kaplan_meier_estimator(defaults, censures, bins=100):
+    times = np.linspace(min(defaults), max(defaults), bins)
+    ordered_times = sorted(defaults)
+    survival_estimation = {x : 0 for x in ordered_times}
+    failures_tk = []
+    at_risks = []
+    for t in ordered_times:
+        failures_tk += [np.sum([censures[k] for k in range(len(defaults)) if defaults[k] == t])]
+        at_risks += [np.sum([1 for k in range(len(defaults)) if defaults[k] > t])]
+    for t in ordered_times:
+        survival_estimation[t] = np.prod([1-(failures_tk[k]/at_risks[k]) for k in range(ordered_times.index(t))])
+    return survival_estimation
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    intensity = 0.2
+    np.random.seed(1)
+    defaults, censures = generate_default_times(intensity=intensity)
+    print("true value : ", intensity)
+    print("MLE estimator without censorship : ", len(defaults)/np.sum(defaults))
+    print("MLE estimator with censorship : ", np.sum(censures)/np.sum(defaults))
+    survival_estimators = kaplan_meier_estimator(defaults, censures)
+    plt.figure()
+    plt.plot(list(survival_estimators.keys()), list(survival_estimators.values()), label="KM")
+    plt.plot(list(survival_estimators.keys()), [np.exp(-(np.sum(censures)/np.sum(defaults))*list(survival_estimators.keys())[k]) for k in range(len(survival_estimators.keys()))], label="MLE estimation with censure")
+    plt.plot(list(survival_estimators.keys()), [np.exp(-(len(defaults)/np.sum(defaults))*list(survival_estimators.keys())[k]) for k in range(len(survival_estimators.keys()))], label="MLE estimation without censure")
+    plt.plot(list(survival_estimators.keys()), [np.exp(-0.2*list(survival_estimators.keys())[k]) for k in range(len(survival_estimators.keys()))], label="real function")
+    plt.legend()
+    plt.title("Survival function estimations")
+    plt.show()
---
+++ optimize.py
@@ -0,0 +1,45 @@
+import numpy as np
+class gen_algo:
+    def __init__(self, pop_size, n_params, func, alpha, mutate_rate):
+        self.pop_size = pop_size
+        self.n_params = n_params
+        self.func = func
+        self.alpha = alpha
+        self.mutate_rate = mutate_rate
+        self.fitness_function = lambda x: x
+        self.current_gen = 0
+        self.init_pop()
+
+    def evaluate_fitness(self):
+        self.current_fitness = [self.fitness_function(self.func([p for p in x])) for x in self.current_pop]
+
+    def init_pop(self):
+        self.current_pop = [[np.random.randint(-10, 10)*np.random.random() for _ in range(self.n_params)] for _ in range(self.pop_size)]
+
+    def generate_next_gen(self):
+        ordered_fitness = sorted(self.current_fitness)
+        best_params = ordered_fitness[:int(self.pop_size*self.alpha)]
+        new_pop = [self.current_pop[self.current_fitness.index(param)] for param in best_params]
+
+        for _ in range(self.pop_size - int(self.pop_size*self.alpha)):
+            pop = new_pop[np.random.randint(0, len(new_pop) - 1)]
+            mom = new_pop[np.random.randint(0, len(new_pop) - 1)]
+            if np.random.random() < self.mutate_rate:
+                pop = [p + np.random.random()*np.random.randint(-50, 50) for p in pop]
+            son = [pop[k] if np.random.random() < 0.5 else mom[k] for k in range(len(pop))]
+            new_pop += [son]
+
+        assert len(new_pop) == self.pop_size
+        self.current_pop = new_pop
+
+def f(params):
+    return 2*params[0]**2 - params[0] + 3*params[1]**3 + 2*params[1]**(4)
+
+if __name__ == "__main__":
+    gen_algo = gen_algo(1000, 2, f, 0.5, 0.05)
+    for _ in range(10):
+        print(gen_algo.current_pop)
+        gen_algo.evaluate_fitness()
+        print(np.min(gen_algo.current_fitness))
+        gen_algo.generate_next_gen()
+        gen_algo.evaluate_fitness()
